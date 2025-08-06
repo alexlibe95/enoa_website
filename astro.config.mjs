c3bff9faa85ded32675243ce75e7b12ed332a77e
alexlibe95 <alexlibe95@gmail.com>
2025-08-06 22:28:35 +0300

    SEO best practices

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,5 +12,31 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [mdx(), sitemap()]
+  integrations: [
+    mdx(), 
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+      customPages: [
+        'https://www.enoa.gr/',
+        'https://www.enoa.gr/about/',
+        'https://www.enoa.gr/activities/',
+        'https://www.enoa.gr/news/',
+        'https://www.enoa.gr/gallery/',
+        'https://www.enoa.gr/contact/',
+        'https://www.enoa.gr/sports/rowing/',
+        'https://www.enoa.gr/sports/sailing/',
+        'https://www.enoa.gr/sports/canoe-kayak/',
+        'https://www.enoa.gr/sports/swimming/',
+        'https://www.enoa.gr/regattas/'
+      ],
+      i18n: {
+        defaultLocale: 'el',
+        locales: {
+          el: 'el-GR'
+        }
+      }
+    })
+  ]
 });
